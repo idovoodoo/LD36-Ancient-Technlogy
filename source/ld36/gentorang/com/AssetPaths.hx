8fa614a707f32f5405b75151ecd7f199625ce325
idovoodoo <svickers.mobile@gmail.com>
2016-08-27 13:52:40 +0100

    Haxe project setup

---
+++ source/ld36/gentorang/com/AssetPaths.hx
@@ -0,0 +1,4 @@
+package ld36.gentorang.com;
+
+@:build(flixel.system.FlxAssets.buildFileReferences("assets", true))
+class AssetPaths {}
---
+++ source/ld36/gentorang/com/Main.hx
@@ -0,0 +1,13 @@
+package ld36.gentorang.com;
+
+import flixel.FlxGame;
+import openfl.display.Sprite;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+		addChild(new FlxGame(640, 480, MenuState));
+	}
+}
